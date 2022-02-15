68a94d722df12738cb953b9e00a561166faadb9c
Jan Wadolowski <jan.wadolowski@stxnext.pl>
2022-02-15 13:24:33 +0100

    Add jinja2 template draft

--- src/sutta_publisher/publishers/html.py
+++ src/sutta_publisher/publishers/html.py
@@ -1,5 +1,7 @@
 import logging
 
+import jinja2
+
 from sutta_publisher.shared.value_objects.results import IngestResult
 
 from .base import Publisher
@@ -12,3 +14,17 @@ class HtmlPublisher(Publisher):
     def publish(cls, result: IngestResult) -> None:
         log.info("** Publishing results: %s", result)
         log.info("** Finished publishing results")
+
+    @staticmethod
+    def render_jinja_html(
+        template_loc: str = "sutta_publisher/templates",
+        file_name: str = "single_page_basic_template.html",
+        **context: dict,
+    ) -> str:
+        return (
+            jinja2.Environment(
+                autoescape=jinja2.select_autoescape(["html"]), loader=jinja2.FileSystemLoader(template_loc + "/")
+            )
+            .get_template(file_name)
+            .render(context)
+        )

df231ce56238907d29602941a1b837a0155cc555
Jan Wadolowski <jan.wadolowski@stxnext.pl>
2022-02-18 15:43:48 +0100

    Add basic structure

---
+++ src/sutta_publisher/ingester/json_parser.py
@@ -0,0 +1,8 @@
+class JsonParser:
+    """Parser for a JSON object creating content of a base HTML <head></head><body>{content}</body>"""
+
+    def __init__(self, json: dict[str, str]) -> None:
+        pass
+
+    def parse_data(self) -> str:
+        pass

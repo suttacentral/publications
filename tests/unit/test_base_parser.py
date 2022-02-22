f0ab901d4be29f8a428f3ff19c9e8dd38e393066
Jan Wadolowski <jan.wadolowski@stxnext.pl>
2022-02-22 04:02:36 +0100

    Sandbox

---
+++ tests/unit/test_base_parser.py
@@ -0,0 +1,35 @@
+import json
+
+import pytest
+
+from sutta_publisher.edition_parsers.base import EditionParser
+from sutta_publisher.shared.config import PAYLOADS_PATH
+
+
+@pytest.fixture
+def payload() -> dict:
+    return json.load(open(PAYLOADS_PATH / "mn-en-sujato_scpub3-ed6-html_2022-02-10-mn.json"))
+
+
+@pytest.fixture
+def base_parser() -> EditionParser:
+    pass
+
+
+# this is more of a sandbox than a test
+def test_base_parser(payload, list_of_all_refs):
+    volumes: list[dict] = [volume for volume in payload]
+    output: list[str] = []
+    for volume in volumes:
+        volume_html: str = []
+        volume_text: dict[str, str] = volume.get("mainmatter").get("main_text") if not None else {}
+        volume_markup: dict[str, str] = volume.get("mainmatter").get("markup")
+        volume_references: dict[str, str] = volume.get("mainmatter").get("reference")
+        # for segment_id in volume_text.keys():
+        #     _process_a_line(
+        #         markup=volume_markup[segment_id],
+        #         segment_id=segment_id,
+        #         text=volume_text[segment_id],
+        #         references=volume_references[segment_id],
+        #         possible_refs=list_of_all_refs,
+        #     )

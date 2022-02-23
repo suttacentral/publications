01f47edfbc1c150c077fedf922dcd20c6ea6d21f
sskoczyl <97095664+sskoczyl@users.noreply.github.com>
2022-02-23 11:26:58 +0100

    Merge 16-publishing-publications into main

---
+++ src/sutta_publisher/shared/github_handler.py
@@ -0,0 +1,59 @@
+import json
+import logging
+from base64 import b64encode
+from io import BytesIO
+
+import requests
+
+log = logging.getLogger(__name__)
+
+
+def upload_file_to_repo(file_path: str, file: BytesIO, repo_url: str, api_key: str) -> None:
+    """Uploads given file to SuttaCentral's editions repository.
+    Parameters:
+        file_path (string): path where file is to be uploaded (e.g. /folder1/folder2/file.html)
+        file (BytesIO): File-like object
+        repo_url (string): url of SuttaCentral editions repo
+        api_key (string): personal token of bot gh account
+    """
+    headers = __get_request_headers(api_key)
+    body = __get_request_body(file, file_path, repo_url)
+
+    request = requests.put(repo_url.format(file_name=file_path), data=json.dumps(body), headers=headers)
+    request.raise_for_status()
+
+    log.info("** Publication uploaded to repo")
+
+
+def __get_request_headers(api_key: str) -> dict:
+    """Creates request headers for GitHub API."""
+
+    return {"Authorization": f"token {api_key}"}
+
+
+def __get_request_body(file: BytesIO, file_path: str, repo_url: str) -> dict:
+    """Creates request body for GitHub API.
+    Parameters:
+        file_path (string): path where file is to be uploaded (e.g. /folder1/folder2/file.html)
+        file (BytesIO): File-like object
+        repo_url (string): url of SuttaCentral editions repo
+    """
+
+    return {
+        "message": f"Uploading {file_path}",
+        "content": b64encode(file.read()).decode("ascii"),
+        "sha": __get_file_sha(file_path, repo_url),
+    }
+
+
+def __get_file_sha(file_path: str, repo_url: str) -> str:
+    """Return sha for existing file. If file does not exist returns empty string.
+    Parameters:
+        file_path (string): path where file is to be uploaded (e.g. /folder1/folder2/file.html)
+        repo_url (string): url of SuttaCentral editions repo
+    """
+
+    response = requests.get(repo_url.format(file_name=file_path))
+    sha_val = response.json().get("sha") or ""
+
+    return sha_val
---
+++ tests/unit/test_github_handler.py
@@ -0,0 +1,42 @@
+from base64 import b64encode
+from io import BytesIO, StringIO
+
+import pytest
+
+from sutta_publisher.shared import github_handler
+
+
+def test_generate_request_headers(bot_api_key: str):
+    header = github_handler.__get_request_headers(bot_api_key)
+
+    assert header["Authorization"] == f"token {bot_api_key}"
+
+
+def test_generate_request_body(monkeypatch, file_like_edition: BytesIO, edition_path_in_repo: str, repo_url: str):
+    monkeypatch.setattr(github_handler, "__get_file_sha", lambda *args: "someshanumber")
+
+    body = github_handler.__get_request_body(file_like_edition, edition_path_in_repo, repo_url)
+
+    file_like_edition.seek(0)
+
+    assert body["message"] == f"Uploading {edition_path_in_repo}"
+    assert body["content"] == b64encode(file_like_edition.read()).decode("ascii")
+    assert body["sha"] == "someshanumber"
+
+
+def test_raise_attribute_error(monkeypatch, edition_path_in_repo: str, repo_url: str):
+    monkeypatch.setattr(github_handler, "__get_file_sha", lambda *args: "someshanumber")
+
+    file_content = "file_content"
+
+    with pytest.raises(AttributeError):
+        github_handler.__get_request_body(file_content, edition_path_in_repo, repo_url)
+
+
+def test_raise_type_error(monkeypatch, edition_path_in_repo: str, repo_url: str):
+    monkeypatch.setattr(github_handler, "__get_file_sha", lambda *args: "someshanumber")
+
+    file_content = StringIO("file_content")
+
+    with pytest.raises(TypeError):
+        github_handler.__get_request_body(file_content, edition_path_in_repo, repo_url)

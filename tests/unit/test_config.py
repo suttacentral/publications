32f6b6b4730cb669872885e28ce8719fc364878a
sskoczyl <szymon.skoczylas@stxnext.pl>
2022-03-01 11:30:03 +0100

    WIP working tests

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -4,13 +4,14 @@ from sutta_publisher.shared.config import get_editions_configs
 from sutta_publisher.shared.value_objects.edition import EditionType
 
 
+@pytest.mark.vcr()
 def test_should_create_config():
     publication_number = "scpub3"
     # When
     editions = get_editions_configs(publication_number=publication_number)
 
     # Then
-    assert len(editions) == 2
+    assert len(editions) == 3
 
     html_edition = editions.get_edition(edition=EditionType.html)
     assert html_edition.edition.publication_type == EditionType.html
@@ -33,9 +34,10 @@ def test_should_create_config():
 
     epub_edition = editions.get_edition(edition=EditionType.epub)
     assert epub_edition.edition.publication_type == EditionType.epub
-    assert len(epub_edition.edition.volumes) == 3
+    assert len(epub_edition.edition.volumes) == 1
 
 
+@pytest.mark.vcr()
 def test_should_raise_for_missing_publication_number():
     publication_number = "missing_pub_no"
 

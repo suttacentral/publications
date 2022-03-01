d913060b5494d250ac3ba6b58fbf922e2792f21e
sskoczyl <97095664+sskoczyl@users.noreply.github.com>
2022-03-01 13:29:06 +0100

    30 api integration (#31)
    Integration with API

--- src/sutta_publisher/shared/config.py
+++ src/sutta_publisher/shared/config.py
@@ -4,27 +4,35 @@ import logging
 from pathlib import Path
 from typing import cast
 
+import requests
+from pydantic import ValidationError
+
 from sutta_publisher.shared.value_objects.edition_config import EditionConfig, EditionMappingList, EditionsConfigs
 
 PAYLOADS_PATH = Path(__file__).parent / "example_payloads"
+API_URL = "http://localhost:80/api/"  # TODO: Change url for real one
+API_ENDPOINTS = {
+    "editions_mapping": "publication/editions",
+    "specific_edition": "publication/edition/{edition_id}",
+}
 
 
 def get_editions_ids(publication_number: str) -> list[str]:
     """Get the editions that are for given `publication_number`."""
-    # TODO: [#20] Get data from real api
-    f_path = PAYLOADS_PATH / "publication_editions_mapping.json"
-    with open(f_path) as f:
-        payload = f.read()
+    response = requests.get(API_URL + API_ENDPOINTS["editions_mapping"])
+    response.raise_for_status()
+    payload = response.content
+
     editions = EditionMappingList.parse_raw(payload)
     return cast(list[str], editions.get_editions_id(publication_number=publication_number))
 
 
 def get_edition_config(edition_id: str) -> EditionConfig:
     """Fetch config for a given edition."""
-    # TODO: [#20] Get data from real api
-    f_path = PAYLOADS_PATH / f"{edition_id}.json"
-    with open(f_path) as f:
-        payload = f.read()
+    response = requests.get(API_URL + API_ENDPOINTS["specific_edition"].format(edition_id=edition_id))
+    response.raise_for_status()
+    payload = response.content.decode("utf-8")
+
     config = EditionConfig.parse_raw(payload)
     return config
 
@@ -37,9 +45,8 @@ def get_editions_configs(publication_number: str) -> EditionsConfigs:
     for each_id in editions_id:
         try:
             editions_config.append(get_edition_config(edition_id=each_id))
-        except FileNotFoundError:
-            msg = "No edition config found for edition_id=%s, skipping that edition."
-            logging.warning(msg, each_id)
+        except ValidationError:
+            logging.warning("Not upported edition type found. Skipping to next one.")
 
     if not editions_config:
         raise SystemExit(f"No valid edition configs found for {publication_number=}. Stopping.")

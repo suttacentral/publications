0642aac90e479824670c3014b4d324d6b800d502
Marek Bryling <marek@bryling.pl>
2022-03-03 11:03:43 +0100

    33 Epub edition

--- src/sutta_publisher/edition_parsers/epub.py
+++ src/sutta_publisher/edition_parsers/epub.py
@@ -1,7 +1,30 @@
+import logging
+import tempfile
+
+from subprocess import Popen, PIPE
+
 from sutta_publisher.shared.value_objects.edition import EditionType
 
 from .base import EditionParser
 
+log = logging.getLogger(__name__)
 
 class EpubEdition(EditionParser):
     edition_type = EditionType.epub
+
+    def __generate_epub(self) -> str:
+        """Generate epub"""
+        log.info("Generating epub...")
+        _html = self._EditionParser__generate_html()
+        _, _tmpfile = tempfile.mkstemp(prefix='raw_', suffix='.html')
+
+        with open(_tmpfile, "w") as _file:
+            _file.write(_html)
+
+            epub = f"{_tmpfile}.epub"
+            pandoc = "pandoc"
+
+        cmd = f"{pandoc} {_tmpfile} -f html -t epub3 -o {epub}"
+
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+        stdout, stderr = p.communicate()

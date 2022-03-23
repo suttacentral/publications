537239646bf654e57f87b82b5b61df8c9ed9445a
Jan Wadolowski <jan.wadolowski@stxnext.pl>
2022-03-23 04:45:40 +0100

    Major rework of ToC generation (collecting headings) in EditionParser (base)

--- sutta_publisher/src/sutta_publisher/edition_parsers/base.py
+++ sutta_publisher/src/sutta_publisher/edition_parsers/base.py
@@ -4,11 +4,16 @@ import ast
 import logging
 import re
 from abc import ABC
-from typing import List, Type
+from typing import Type
 
 import requests
+from bs4 import BeautifulSoup, Tag
+from bs4.element import ResultSet
 
-from sutta_publisher.edition_parsers.helper_functions import _fetch_possible_refs, _process_a_line
+from sutta_publisher.edition_parsers.helper_functions import (
+    _fetch_possible_refs,
+    _process_a_line, collect_main_toc_depths, _collect_headings, MAX_HEADING_DEPTH,
+)
 from sutta_publisher.shared.value_objects.edition import EditionResult, EditionType
 from sutta_publisher.shared.value_objects.edition_config import EditionConfig
 from sutta_publisher.shared.value_objects.edition_data import EditionData
@@ -22,35 +27,21 @@ class EditionParser(ABC):
     edition_type: EditionType
 
     def __init__(self, config: EditionConfig, data: EditionData) -> None:
-        self.raw_data = data
-        self.config = config
-        self.possible_refs = _fetch_possible_refs()
+        self.raw_data: EditionData = data
+        self.config: EditionConfig = config
+        self.possible_refs: list[str] = _fetch_possible_refs()
+        self.per_volume_html: list[BeautifulSoup] = EditionParser.__generate_html(
+            raw_data=data, possible_refs=self.possible_refs
+        )
 
-    def __convert_descriptive_toc_to_int(self, text: str) -> int:
-        if text == "all":
-            return 6
-        elif match := re.match("^[1-6]$", text):
-            return int(match.group(0))
-        else:
-            raise ValueError("Provided depth '%s' not supported", text)
-
-    def __get_toc_depth(self) -> int:
-        """Return depth of the table of contents for the whole publication (main ToC)"""
-        return self.__convert_descriptive_toc_to_int(self.config.edition.main_toc_depth)
-
-    def __collect_main_toc_headings(self) -> list[str]:
-        pass
-
-    def __collect_secondary_toc_headings(self) -> list[str]:
-        pass
-
-    def __generate_html(self) -> List[str]:
+    @staticmethod
+    def __generate_html(raw_data: EditionData, possible_refs: list[str]) -> list[BeautifulSoup]:
         """Generate content of an HTML body"""
         log.debug("Generating html...")
 
         # Publication is separated into volumes
         publication_html_volumes_output: list[str] = []
-        for volume in self.raw_data:
+        for volume in raw_data:
             # Each volume's mainmatter is separated into a list of mainmatter sub-entities (MainMatterDetails class)
             processed_single_mainmatter_subentity: list[str] = []
             for mainmatter_info in volume.mainmatter:
@@ -68,7 +59,7 @@ class EditionParser(ABC):
                                 text=mainmatter_info.mainmatter.main_text.get(segment_id, ""),
                                 references=mainmatter_info.mainmatter.reference.get(segment_id, ""),
                                 # references are provides as: "single, comma, separated, string". We take care of splitting it in helper functions
-                                possible_refs=self.possible_refs,
+                                possible_refs=possible_refs,
                             )
                         )
                 except AttributeError:
@@ -87,7 +78,36 @@ class EditionParser(ABC):
             publication_html_volumes_output.append(
                 single_mainmatter_output
             )  # all main matters from each volumes as a list of html bodies' contents
-        return publication_html_volumes_output
+        return [BeautifulSoup(volume, "lxml") for volume in publication_html_volumes_output]
+
+    def __collect_main_toc_headings(self) -> None:
+        log.debug("Collecting headings for the main ToC...")
+        depth: str = self.config.edition.main_toc_depth
+        per_volume_depth: list[int] = collect_main_toc_depths(depth=depth, all_volumes=self.per_volume_html)
+        self.main_toc_headings: list[list[Tag]] = []
+        for depth_, volume_ in zip(per_volume_depth, self.per_volume_html):
+            self.main_toc_headings.append(_collect_headings(end_depth=depth_, volume=volume_))
+
+    def __collect_secondary_toc_targets(self) -> list[Tag]:
+        # TODO: implement
+        pass
+
+    def __collect_secondary_toc_headings(self) -> None:
+        log.debug("Collecting headings for secondary ToCs...")
+        depth: str = self.config.edition.main_toc_depth
+        per_volume_targets = self.__collect_secondary_toc_targets()
+        per_volume_depth: list[int] = []
+        for i in collect_main_toc_depths(depth=depth, all_volumes=self.per_volume_html):
+            per_volume_depth.append(i + 1 if i < MAX_HEADING_DEPTH else i)  # to avoid having start_depth > end_depth
+        self.secondary_toc_headings: dict[Tag, list[Tag]] = {}
+        for target_, depth_, volume_ in zip(per_volume_targets, per_volume_depth, self.per_volume_html):
+            self.secondary_toc_headings.update(
+                {target_: _collect_headings(start_depth=depth_, end_depth=MAX_HEADING_DEPTH, volume=volume_)})
+
+    def __generate_toc(self) -> None:
+        self.__collect_main_toc_headings()
+        if self.config.edition.secondary_toc:
+            self.__collect_secondary_toc_headings()
 
     def __generate_frontmatter(self) -> dict[str, str]:
         log.debug("Generating covers...")
@@ -118,7 +138,8 @@ class EditionParser(ABC):
                     matters_dict["acknowledgements"] = response.text
 
                 case _:
-                    raise ValueError("Not supported frontmatter type")
+                    # raise ValueError("Not supported frontmatter type")
+                    pass
 
         return matters_dict
 
@@ -129,7 +150,7 @@ class EditionParser(ABC):
         log.debug("Generating end matters...")
 
     def collect_all(self) -> EditionResult:
-        self.__generate_html()
+        self.__generate_html(raw_data=self.raw_data, possible_refs=self.possible_refs)
         self.__generate_frontmatter()
         self.__generate_endmatter()
         self.__generate_covers()

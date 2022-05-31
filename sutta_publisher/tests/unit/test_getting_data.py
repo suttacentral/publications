82b558eeb530378ba025324c580e5fa4ab931ef7
Wojciech Sidorowicz <wojsidorowicz@gmail.com>
2022-05-31 16:12:46 +0200

    Update variable name to be more precise

--- sutta_publisher/tests/unit/test_getting_data.py
+++ sutta_publisher/tests/unit/test_getting_data.py
@@ -23,17 +23,17 @@ def test_should_return_data_for_edition() -> None:
     assert first_mainmatter_node.name == "Middle Discourses Collection"
     assert first_mainmatter_node.type == "branch"
 
-    some_mainmatter_chunk = first_edition_data.mainmatter[0][20]
-    assert some_mainmatter_chunk.blurb == (
+    some_mainmatter_node = first_edition_data.mainmatter[0][20]
+    assert some_mainmatter_node.blurb == (
         "While living in the wilderness is great, not everyone is ready for it. "
         "The Buddha encourages meditators to reflect on whether one’s environment is genuinely supporting their meditation practice, "
         "and if not, to leave."
     )
-    assert set(some_mainmatter_chunk.mainmatter.main_text) & {"mn17:0.1", "mn17:0.2", "mn17:1.1", "mn17:1.2"}
-    assert set(some_mainmatter_chunk.mainmatter.markup) & {"mn17:0.1", "mn17:0.2", "mn17:1.1", "mn17:1.2"}
-    assert set(some_mainmatter_chunk.mainmatter.reference) & {"mn17:1.1", "mn17:10.1", "mn17:11.1", "mn17:12.1"}
-    assert some_mainmatter_chunk.name == "Jungle Thickets "
-    assert some_mainmatter_chunk.type == "leaf"
+    assert set(some_mainmatter_node.mainmatter.main_text) & {"mn17:0.1", "mn17:0.2", "mn17:1.1", "mn17:1.2"}
+    assert set(some_mainmatter_node.mainmatter.markup) & {"mn17:0.1", "mn17:0.2", "mn17:1.1", "mn17:1.2"}
+    assert set(some_mainmatter_node.mainmatter.reference) & {"mn17:1.1", "mn17:10.1", "mn17:11.1", "mn17:12.1"}
+    assert some_mainmatter_node.name == "Jungle Thickets "
+    assert some_mainmatter_node.type == "leaf"
 
     assert set(first_edition_data.extras) & {
         "./matter/acknowledgements.html",

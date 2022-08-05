b99d811ddd58b8a0cfedf907f3ac725a70be27b0
Wojciech Sidorowicz <wojsidorowicz@gmail.com>
2022-08-05 10:05:53 +0200

    Set front, main and backmatter, translate html tags into Latex syntax

--- sutta_publisher/src/sutta_publisher/edition_parsers/pdf.py
+++ sutta_publisher/src/sutta_publisher/edition_parsers/pdf.py
@@ -1,13 +1,13 @@
 import logging
 import os.path
 import tempfile
-from typing import Callable
+from typing import Callable, cast
 
 from bs4 import BeautifulSoup, NavigableString, PageElement, Tag
-from pylatex import Document, NewPage, NoEscape, Section, Tabular
-from pylatex.base_classes import Command, Environment, UnsafeCommand
+from pylatex import Document, NewPage, NoEscape
+from pylatex.base_classes import Command, Environment
 from pylatex.package import Package
-from pylatex.section import Chapter
+from pylatex.utils import italic
 
 from sutta_publisher.shared.value_objects.edition import EditionResult, EditionType
 from sutta_publisher.shared.value_objects.parser_objects import Edition, Volume
@@ -17,126 +17,180 @@ from .base import EditionParser
 log = logging.getLogger(__name__)
 
 
-class AsideEnv(Environment):
-    _latex_name = "aside"
-    packages = [Package("mdframed")]
-
-
 class PdfEdition(EditionParser):
     edition_type = EditionType.pdf
+    endnotes: list[str] | None
 
-    def _process_tag(self, doc: Document, tag: Tag) -> None:
-        if tag.has_attr("class") and any(_class in tag["class"] for _class in ["range-title", "sutta-title"]):
-            # acronym, trans_title, root_title = tag.stripped_strings
-            doc.append(Section(title=" ".join(tag.stripped_strings), numbering=False, label=tag.parent.id))
+    def _append_paragraph(self, doc: Document, tag: Tag) -> str:
+        tex: str = ""
+        if tag.has_attr("id"):
+            tex += Command("marginnote", tag["id"], tag["id"]).dumps()
+        tex += self._process_contents(doc=doc, contents=tag.contents)
+        if tag.next_sibling:
+            tex += NoEscape("\n\n")
+        return tex
 
-        elif tag.has_attr("class") and "section-title" in tag["class"]:
-            doc.append(NewPage())
-            doc.append(Chapter(title=tag.string.strip(), numbering=False, label=tag.id))
+    def _append_verse(self, doc: Document, tag: Tag) -> str:
+        verse: Environment = Environment()
+        verse._latex_name = "verse"
+        _data: str = self._process_contents(doc=doc, contents=tag.contents)
+        verse.append(NoEscape(_data))
+        return cast(str, verse.dumps())
 
-        elif tag.name == "br":
-            doc.append(NoEscape(r"\\"))
+    def _append_quotation(self, doc: Document, tag: Tag) -> str:
+        quotation: Environment = Environment()
+        quotation._latex_name = "quotation"
+        _data: str = self._process_contents(doc=doc, contents=tag.contents)
+        quotation.append(NoEscape(_data))
+        return cast(str, quotation.dumps())
 
-        elif tag.name == "p":
-            self._process_contents(doc=doc, contents=tag.contents)
-            if tag.next_sibling and not tag.next_sibling.name == "blockquote":
-                doc.append(NoEscape(r"\\"))
+    @staticmethod
+    def _append_breakline() -> str:
+        return cast(str, NoEscape(r"\\") + NoEscape("\n"))
+
+    @staticmethod
+    def _append_emphasis(tag: Tag) -> str:
+        return cast(str, NoEscape(rf"\emph{{{tag.string}}}"))
+
+    @staticmethod
+    def _append_roman_script_macro(tag: Tag) -> str:
+        return cast(str, italic(tag.string))
+
+    @staticmethod
+    def _append_other_script_macro(tag: Tag) -> str:
+        return cast(str, NoEscape(rf'\text{tag["lang"]}{{{tag.string}}}'))
+
+    def _append_footnote(self, doc: Document, tag: Tag) -> str:
+        if self.endnotes:
+            _index: int = int(tag.string)
+            _note_contents: list[PageElement] = BeautifulSoup(self.endnotes[_index - 1], "lxml").body.contents
+            _data: str = self._process_contents(doc=doc, contents=_note_contents)
+            return cast(str, Command("footnote", NoEscape(_data)).dumps())
+        else:
+            return ""
+
+    @staticmethod
+    def _append_section(tag: Tag) -> str:
+        tex: str = ""
+        _title: str = " ".join(tag.stripped_strings)
+        tex += Command("section*", _title).dumps() + NoEscape("\n")
+        tex += Command("addcontentsline", arguments=["toc", "section", _title]).dumps() + NoEscape("\n")
+        tex += Command("markboth", arguments=[_title, _title]).dumps() + NoEscape("\n")
+        return tex
+
+    @staticmethod
+    def _append_chapter(tag: Tag) -> str:
+        tex: str = ""
+        _title: str = tag.string
+        tex += Command("chapter*", _title).dumps() + NoEscape("\n")
+        tex += Command("addcontentsline", arguments=["toc", "chapter", _title]).dumps() + NoEscape("\n")
+        tex += Command("markboth", arguments=[_title, _title]).dumps() + NoEscape("\n")
+        return tex
+
+    @staticmethod
+    def _append_new_page() -> str:
+        return cast(str, NewPage().dumps())
+
+    @staticmethod
+    def _append_tableofcontents() -> str:
+        return cast(str, Command("tableofcontents").dumps())
+
+    def _append_epigraph(self, doc: Document, tag: Tag) -> str:
+        _text_tag, _source_tag = tag.find_all("p")
+        _text: str = NoEscape(self._process_tag(doc=doc, tag=_text_tag))
+        _source: str = italic(NoEscape(self._process_tag(doc=doc, tag=_source_tag)))
+        return cast(str, Command("epigraph", arguments=[_text, _source]).dumps())
+
+    def _process_tag(self, doc: Document, tag: Tag) -> str:
+        if tag.name == "p":
+            return self._append_paragraph(doc, tag)
 
         elif tag.name == "blockquote":
-            with doc.create(Environment()) as _env:
-                _env._latex_name = "verse"
-                self._process_contents(doc=doc, contents=tag.contents)
+            if tag.has_attr("class") and "gatha" in tag["class"]:
+                return self._append_verse(doc, tag)
+            else:
+                return self._append_quotation(doc, tag)
 
-        elif tag.name == "aside":
-            with doc.create(AsideEnv()):
-                self._process_contents(doc=doc, contents=tag.contents)
+        elif tag.name == "br":
+            return self._append_breakline()
 
-        elif tag.name == "table":
-            _rows: int = max(len([_td for _td in _tr.find_all("td")]) for _tr in tag.find_all("tr"))
-            _table_spec: str = " ".join("l" for _ in range(_rows))
-            _has_head: bool = bool(tag.find("th"))
+        elif tag.name == "em":
+            return self._append_emphasis(tag)
 
-            with doc.create(Tabular(table_spec=_table_spec)) as _table:
-                if _has_head:
-                    _table.add_row([_th.string for _th in tag.find_all("th")])
-                    _table.end_table_header()
-                for _row in tag.find_all("tr"):
-                    _table.add_row([_td.string for _td in _row.find_all("td")])
-            doc.append(NoEscape(r"\\"))
+        elif tag.name == "i":
+            if tag.has_attr("lang") and any(_lang in tag["lang"] for _lang in ["pi", "san"]):
+                return self._append_roman_script_macro(tag)
+            else:
+                return self._append_other_script_macro(tag)
 
-        elif tag.name == "address":
-            doc.append(
-                Command(
-                    "address",
-                    NoEscape(
-                        r"\\".join(_el for _el in tag.contents if isinstance(_el, NavigableString) and _el != "\n")
-                    ),
-                )
-            )
+        elif tag.has_attr("role") and "doc-noteref" in tag["role"]:
+            return self._append_footnote(doc, tag)
+
+        elif tag.name == "h2" and tag.has_attr("class") and "sutta-title" in tag["class"]:
+            return self._append_section(tag)
+
+        elif tag.name == "h1":
+            return self._append_chapter(tag)
+
+        elif tag.has_attr("id") and tag["id"] in self.new_pages:
+            return self._append_new_page()
+
+        elif tag.has_attr("id") and tag["id"] == "main-toc":
+            return self._append_tableofcontents()
+
+        elif tag.has_attr("class") and "epigraph" in tag["class"]:
+            return self._append_epigraph(doc, tag)
 
         else:
-            self._process_contents(doc=doc, contents=tag.contents)
+            return self._process_contents(doc=doc, contents=tag.contents)
 
-    def _process_contents(self, doc: Document, contents: list[PageElement]) -> None:
+    def _process_contents(self, doc: Document, contents: list[PageElement]) -> str:
+        tex: str = ""
+
         for _element in contents:
             if isinstance(_element, Tag):
-                self._process_tag(doc=doc, tag=_element)
+                tex += self._process_tag(doc=doc, tag=_element)
             elif isinstance(_element, NavigableString) and _element != "\n":
-                doc.append(_element)
-            else:
-                pass
+                tex += _element
+
+        return tex
 
     def _generate_latex(self, volume: Volume) -> Document:
+        # setup
+        self.endnotes: list[str] | None = volume.endnotes if volume.endnotes else None
+        self.new_pages: list[str] = ["imprint", "halftitlepage"]
+
         doc = Document(documentclass="book")
 
         # set packages
-        doc.packages.append(Package("mdframed"))
-        doc.packages.append(Package("verse"))
-
-        # set additional commands
-        doc.append(
-            UnsafeCommand(
-                "newenvironment",
-                "aside",
-                extra_arguments=[
-                    r"\begin{mdframed}[style=0,leftline=false,rightline=false,topline=false,bottomline=false,leftmargin=2em,rightmargin=2em,innerleftmargin=0pt,innerrightmargin=0pt,linewidth=0.75pt,skipabove=7pt,skipbelow=7pt]\small",
-                    r"\end{mdframed}",
-                ],
-            )
-        )
+        doc.packages.append(Package("marginnote"))
 
         # set frontmatter
-        # doc.append(Command("frontmatter"))
-        # for _part in volume.frontmatter:
-        #     _element = BeautifulSoup(_part, "lxml").find("body").next_element
-        #     if isinstance(_element, Tag) and _element.has_attr("id") and _element["id"] == "main-toc":
-        #         continue
-        #     doc.append(NewPage())
-        #     if isinstance(_element, Tag):
-        #         self._process_tag(doc=doc, tag=_element)
-        #     elif isinstance(_element, NavigableString) and _element != "\n":
-        #         doc.append(_element)
-        #     else:
-        #         pass
+        doc.append(Command("frontmatter"))
+        for _part in volume.frontmatter:
+            _element = BeautifulSoup(_part, "lxml").find("body").next_element
+            if isinstance(_element, Tag):
+                doc.append(NoEscape(self._process_tag(doc=doc, tag=_element)))
+            elif isinstance(_element, NavigableString) and _element != "\n":
+                doc.append(_element)
 
         # set mainmatter
         doc.append(Command("mainmatter"))
         _mainmatter = BeautifulSoup(volume.mainmatter, "lxml").find("body")
         for _element in _mainmatter:
             if isinstance(_element, Tag):
-                self._process_tag(doc=doc, tag=_element)
-            else:
+                doc.append(NoEscape(self._process_tag(doc=doc, tag=_element)))
+            elif isinstance(_element, NavigableString) and _element != "\n":
                 doc.append(_element)
 
         # set backmatter
-        # doc.append(Command("backmatter"))
-        # for _part in volume.backmatter:
-        #     _matter = BeautifulSoup(_part, "lxml").find("body")
-        #     for _element in _matter:
-        #         if isinstance(_element, Tag):
-        #             self._process_tag(doc=doc, tag=_element)
-        #         else:
-        #             doc.append(_element)
+        doc.append(Command("backmatter"))
+        for _part in volume.backmatter:
+            _element = BeautifulSoup(_part, "lxml").find("body").next_element
+            if isinstance(_element, Tag):
+                doc.append(NoEscape(self._process_tag(doc=doc, tag=_element)))
+            elif isinstance(_element, NavigableString) and _element != "\n":
+                doc.append(_element)
 
         return doc
 
@@ -145,7 +199,8 @@ class PdfEdition(EditionParser):
 
         _path: str = os.path.join(tempfile.gettempdir(), volume.filename[:-4])
         doc = self._generate_latex(volume=volume)
-        doc.generate_pdf(filepath=_path, clean_tex=False, compiler="latexmk")
+        doc.generate_tex(filepath=_path)
+        # doc.generate_pdf(filepath=_path, clean_tex=False, compiler="latexmk")
 
     def collect_all(self):  # type: ignore
         _edition: Edition = super().collect_all()

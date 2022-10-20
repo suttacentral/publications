450711d18d4fe606fdda7d6188105e3d44574268
Wojciech Sidorowicz <wojsidorowicz@gmail.com>
2022-10-20 08:36:44 +0200

    Add queue to GitHub handler (#141)
    * Add a simple queue for GitHub handler requests
    * Add tests

--- sutta_publisher/tests/unit/test_github_handler.py
+++ sutta_publisher/tests/unit/test_github_handler.py
@@ -1,4 +1,7 @@
+from unittest import mock
+
 import pytest
+from requests.models import Response
 
 from sutta_publisher.shared import github_handler
 
@@ -52,5 +55,123 @@ from sutta_publisher.shared import github_handler
     ],
 )
 def test_match_file(filename: str, content: list[dict], expected: dict) -> None:
-    result = github_handler.__match_file(filename, content)
+    result = github_handler._match_file(filename, content)
     assert result == expected
+
+
+@mock.patch("sutta_publisher.shared.github_handler.requests.get")
+def test_worker_success(mock_get) -> None:
+    mock_response = Response()
+    mock_response.status_code = 200
+    mock_get.return_value = mock_response
+
+    request = {
+        "method": "get",
+        "url": "https://example.com/repo_url",
+        "type": "test",
+    }
+    responses = github_handler.worker(request, "test_key")
+    assert len(responses) == 1
+    mock_get.assert_called_once_with(
+        url="https://example.com/repo_url",
+        headers={"Accept": "application/vnd.github+json", "Authorization": "Token test_key"},
+        data=None,
+    )
+
+
+@mock.patch("sutta_publisher.shared.github_handler.requests.get")
+@mock.patch("sutta_publisher.shared.github_handler.sleep")
+def test_worker_success_with_one_fail(mock_sleep, mock_get: mock.Mock) -> None:
+    mock_responses = []
+    for i in range(3):
+        mock_response = Response()
+        mock_response.status_code = 404 if i < 2 else 200
+        mock_responses.append(mock_response)
+
+    mock_get.side_effect = mock_responses
+
+    request = {
+        "method": "get",
+        "url": "https://example.com/repo_url",
+        "type": "test",
+    }
+    responses = github_handler.worker(request, "test_key")
+    assert len(responses) == 1
+    assert mock_get.call_count == 3
+    mock_get.assert_called_with(
+        url="https://example.com/repo_url",
+        headers={"Accept": "application/vnd.github+json", "Authorization": "Token test_key"},
+        data=None,
+    )
+
+
+@mock.patch("sutta_publisher.shared.github_handler.requests.get")
+@mock.patch("sutta_publisher.shared.github_handler.sleep")
+def test_worker_raises(mock_sleep, mock_get) -> None:
+    mock_response = Response()
+    mock_response.status_code = 404
+    mock_get.return_value = mock_response
+
+    request = {
+        "method": "get",
+        "url": "https://example.com/repo_url",
+        "type": "test",
+    }
+
+    with pytest.raises(SystemExit):
+        github_handler.worker(request, "test_key")
+        assert mock_get.call_count == 3
+
+
+@mock.patch("sutta_publisher.shared.github_handler.requests.get")
+@mock.patch("sutta_publisher.shared.github_handler.sleep")
+def test_worker_silent(mock_sleep, mock_get) -> None:
+    mock_response = Response()
+    mock_response.status_code = 404
+    mock_get.return_value = mock_response
+
+    request = {
+        "method": "get",
+        "url": "https://example.com/repo_url",
+        "type": "test",
+    }
+
+    ret = github_handler.worker(request, "test_key", silent=True)
+    assert mock_get.call_count == 3
+    assert ret == []
+
+
+@mock.patch("sutta_publisher.shared.github_handler.requests.get")
+@mock.patch("sutta_publisher.shared.github_handler.sleep")
+def test_worker_return_is_sorted(mock_sleep, mock_get: mock.Mock) -> None:
+    """The order of worker()'s return should be the same as input"""
+    test_data = (
+        (404, 1),
+        (200, 2),
+        (404, 3),
+        (404, 1),
+        (200, 3),
+        (200, 1),
+    )
+
+    mock_responses = []
+    for _status_code, _task_index in test_data:
+        mock_response = Response()
+        mock_response.status_code = _status_code
+        mock_response.task_index = _task_index
+        mock_responses.append(mock_response)
+
+    mock_get.side_effect = mock_responses
+
+    _requests = [
+        {
+            "method": "get",
+            "url": "https://example.com/repo_url",
+            "type": "test",
+        }
+        for _ in range(3)
+    ]
+    responses = github_handler.worker(_requests, "test_key")
+    assert len(responses) == 3
+    assert mock_get.call_count == len(test_data)
+    assert responses == sorted(responses, key=lambda x: x.task_index)
